f759c9960943106f758bf4df1125a01d0b929aed
Andy Williams <andy@andywilliams.me>
2020-08-03 12:30:17 +0100

    Load images as we construct the user interface

--- chapter04/image.go
+++ chapter04/image.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"image/color"
+	"log"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/storage"
 )
+
+func loadImage(u fyne.URI) fyne.CanvasObject {
+	read, err := storage.OpenFileFromURI(u)
+	if err != nil {
+		log.Println("Error opening image", err)
+		return canvas.NewRectangle(color.Black)
+	}
+	res, err := fyne.LoadResourceFromURI(read)
+	if err != nil {
+		log.Println("Error reading image", err)
+		return canvas.NewRectangle(color.Black)
+	}
+
+	img := canvas.NewImageFromResource(res)
+	img.FillMode = canvas.ImageFillContain
+	return img
+}
 
 func makeImageItem(u fyne.URI) fyne.CanvasObject {
 	label := canvas.NewText(u.Name(), color.Gray{128})
 	label.Alignment = fyne.TextAlignCenter
-	img := canvas.NewRectangle(color.Black)
 	return fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, label, nil, nil),
-		label, img)
+		label, loadImage(u))
 }
