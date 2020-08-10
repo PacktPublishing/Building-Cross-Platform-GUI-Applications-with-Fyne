a31a343becee4cb1ab56a616782860aabe836a70
Andy Williams <andy@andywilliams.me>
2020-08-10 12:51:30 +0100

    Add attractive faded overlay for less wasted space
    Using a custom layout

--- chapter04/image.go
+++ chapter04/image.go
@@ -55,9 +55,40 @@ func loadImage(u fyne.URI) fyne.CanvasObject {
 	return img
 }
 
+type itemLayout struct {
+	bg, text, gradient fyne.CanvasObject
+}
+
+func (i *itemLayout) MinSize(_ []fyne.CanvasObject) fyne.Size {
+	return fyne.NewSize(160, 120)
+}
+
+func (i *itemLayout) Layout(objs []fyne.CanvasObject, size fyne.Size) {
+	textHeight := 22
+	for _, o := range objs {
+		if o == i.text {
+			o.Move(fyne.NewPos(0, size.Height-textHeight))
+			o.Resize(fyne.NewSize(size.Width, textHeight))
+		} else if o == i.bg {
+			o.Move(fyne.NewPos(0, size.Height-textHeight))
+			o.Resize(fyne.NewSize(size.Width, textHeight))
+		} else if o == i.gradient {
+			o.Move(fyne.NewPos(0, size.Height-int(float32(textHeight)*1.5)))
+			o.Resize(fyne.NewSize(size.Width, textHeight/2))
+		} else {
+			o.Move(fyne.NewPos(0, 0))
+			o.Resize(size)
+		}
+	}
+}
+
 func makeImageItem(u fyne.URI) fyne.CanvasObject {
 	label := canvas.NewText(u.Name(), color.Gray{128})
 	label.Alignment = fyne.TextAlignCenter
-	return fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, label, nil, nil),
-		label, loadImage(u))
+
+	bgColor := &color.NRGBA{R: 255, G: 255, B: 255, A: 224}
+	bg := canvas.NewRectangle(bgColor)
+	fade := canvas.NewLinearGradient(color.Transparent, bgColor, 0)
+	return fyne.NewContainerWithLayout(&itemLayout{text: label, bg: bg, gradient: fade},
+		loadImage(u), bg, fade, label)
 }
