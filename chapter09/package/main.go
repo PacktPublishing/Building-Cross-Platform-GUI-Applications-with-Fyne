4013434b07806737d8ac62fe706f4b330016970e
Andy Williams <andy@andywilliams.me>
2020-10-15 17:30:23 +0100

    A simple project for testing packaging

---
+++ chapter09/package/main.go
@@ -0,0 +1,22 @@
+// Package main loads a very basic Hello World graphical application.
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(widget.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
