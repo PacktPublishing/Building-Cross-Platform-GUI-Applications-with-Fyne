29a65dd0c1008687f64677b76be28ab03f15228d
Andy Williams <andy@andywilliams.me>
2020-11-03 18:35:57 +0000

    Animate the head and tail of our snake

--- chapter03/example/main.go
+++ chapter03/example/main.go
@@ -25,6 +25,7 @@ const (
 var (
 	snakeParts []snakePart
 	game       *fyne.Container
+	head       *canvas.Rectangle
 	move       = moveUp
 )
 
@@ -66,22 +67,40 @@ func refreshGame() {
 }
 
 func runGame() {
+	nextPart := snakePart{snakeParts[0].x, snakeParts[0].y - 1}
 	for {
+		oldPos := fyne.NewPos(snakeParts[0].x*10, snakeParts[0].y*10)
+		newPos := fyne.NewPos(nextPart.x*10, nextPart.y*10)
+		canvas.NewPositionAnimation(oldPos, newPos, time.Millisecond*250, func(p fyne.Position) {
+			head.Move(p)
+			canvas.Refresh(head)
+		}).Start()
+
+		end := len(snakeParts) - 1
+		canvas.NewPositionAnimation(fyne.NewPos(snakeParts[end].x*10, snakeParts[end].y*10),
+			fyne.NewPos(snakeParts[end-1].x*10, snakeParts[end-1].y*10), time.Millisecond*250,
+			func(p fyne.Position) {
+				tail := game.Objects[end]
+				tail.Move(p)
+				canvas.Refresh(tail)
+			}).Start()
+
 		time.Sleep(time.Millisecond * 250)
 		for i := len(snakeParts) - 1; i >= 1; i-- {
 			snakeParts[i] = snakeParts[i-1]
 		}
+		snakeParts[0] = nextPart
+		refreshGame()
 		switch move {
 		case moveUp:
-			snakeParts[0].y--
+			nextPart = snakePart{nextPart.x, nextPart.y - 1}
 		case moveDown:
-			snakeParts[0].y++
+			nextPart = snakePart{nextPart.x, nextPart.y + 1}
 		case moveLeft:
-			snakeParts[0].x--
+			nextPart = snakePart{nextPart.x - 1, nextPart.y}
 		case moveRight:
-			snakeParts[0].x++
+			nextPart = snakePart{nextPart.x + 1, nextPart.y}
 		}
-		refreshGame()
 	}
 }
 
@@ -97,5 +116,9 @@ func setupGame() *fyne.Container {
 		segments = append(segments, r)
 	}
 
+	head = canvas.NewRectangle(&color.RGBA{G: 0x66, A: 0xff})
+	head.Resize(fyne.NewSize(10, 10))
+	head.Move(fyne.NewPos(snakeParts[0].x*10, snakeParts[0].y*10))
+	segments = append(segments, head)
 	return fyne.NewContainerWithoutLayout(segments...)
 }
